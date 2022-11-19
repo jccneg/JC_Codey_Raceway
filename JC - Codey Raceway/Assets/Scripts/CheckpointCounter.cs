6d5020d44192c5d8838ae879c2af1ef5f376586c
collinmi22 <collinMI@outlook.com>
2022-11-19 12:05:41 -0800

    JC - Codey Raceway (4)

---
+++ JC - Codey Raceway/Assets/Scripts/CheckpointCounter.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointCounter : MonoBehaviour
+{
+    public int numberOfCheckpoints;
+    public int triggeredCheckpoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        numberOfCheckpoints = GameObject.FindGameObjectsWithTag("checkpoint").Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ JC - Codey Raceway/Assets/Scripts/TriggerFinishLine.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerFinishLine : MonoBehaviour
+{
+    public CheckpointCounter checkpointTracker;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (checkpointTracker.triggeredCheckpoints == checkpointTracker.numberOfCheckpoints)
+            {
+                print("You Win!");
+            }
+            else
+            {
+                print("Cheater!");
+            }
+        }
+    }
+}
